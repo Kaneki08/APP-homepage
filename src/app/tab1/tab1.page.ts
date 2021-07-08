30a06fd29b63028879fc36a73b5ddd98a49869c8
Kaneki08 <derickalexander98@gmail.com>
2021-07-08 15:41:43 -0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+declare var google;
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-
+export class Tab1Page implements AfterViewInit{
+  map;
+  @ViewChild ('mapElement', {static: false}) mapElement;
+ mapOptions = {
+   center: { lat: -34.397, lng: 150.644},
+   zoom: 8,
+ }
   constructor() {}
 
+  loadMap() {
+     this.map = new google.maps.Map(this.mapElement.nativeElement, this.mapOptions);
+  }
+ ngAfterViewInit(): void {
+   this.loadMap
+}
 }

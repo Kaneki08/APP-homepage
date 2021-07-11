dc6e1d60a54a2ade6bf14ddbe30de86fcf8a4877
Kaneki08 <derickalexander98@gmail.com>
2021-07-10 19:32:25 -0700

    adding all my map files to working branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)},
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    path: 'planning',
+    loadChildren: () => import('./planning/planning.module').then( m => m.PlanningPageModule)
+  },
+  {
+    path: 'trip',
+    loadChildren: () => import('./trip/trip.module').then( m => m.TripPageModule)
+  },
 ];
 @NgModule({
   imports: [
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 
 import { Component } from '@angular/core';
 import { ViewChild, ElementRef } from '@angular/core'
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 declare var google: any;
 
@@ -12,23 +13,69 @@ declare var google: any;
 export class Tab1Page {
 
   map: any;
+  latitude: number;
+  longitude: number;
 
   @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
 
-  constructor() {}
+  constructor(private geolocation: Geolocation) {}
 
   ionViewDidEnter(){
     this.showMap();
   }
   
-  
+
+
   showMap() {
-    const location = new google.maps.LatLng(-17.824858, 31.053028);
+    const location = new google.maps.LatLng(34.0522, -118.2437);
     const options = {
       center: location,
-      zoom: 15,
+      zoom: 8,
       disableDefaultUI: true
     }
     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
   }
-}
+
+  getUserLocation() {
+    this.geolocation.getCurrentPosition().then((res) => {
+          this.latitude = res.coords.latitude;
+          this.longitude = res.coords.longitude;
+
+          let latLng = new google.maps.LatLng(this.latitude, this.longitude);
+              const options = {
+                center: latLng,
+                zoom: 18,
+                disableDefaultUI: true
+              }
+              this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+                })
+    }
+  }
+
+  // showMap() {
+  //   this.geolocation.getCurrentPosition().then((res) => {
+  //     this.latitude = res.coords.latitude;
+  //     this.longitude = res.coords.latitude;
+      
+  //     let latLng = new google.maps.LatLng(this.latitude, this.longitude);
+  //     const options = {
+  //       center: latLng,
+  //       zoom: 15,
+  //       disableDefaultUI: true
+  //     }
+
+  //     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  //   })
+
+    // var loc = {};
+    // var geocoder = new google.maps.Geocoder();
+    // console.log("hi")
+    // const location = new google.maps.LatLng(-17.824858, 31.053028);
+    // // const location = new google.maps.LatLng(-17.824858, 31.053028);
+    // const options = {
+    //   center: location,
+    //   zoom: 15,
+    //   disableDefaultUI: true
+    // }
+    // this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+
